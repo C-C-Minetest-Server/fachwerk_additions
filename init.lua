dd67b8eab21c195f89c563c07d8cf29a4929f291
sparky4 <sparky4@cock.li>
2024-03-04 21:42:16 -0600

    more additions by Och_Noe

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 -- fachwerk_additions
 --
--- $Id: init.lua,v 1.5 2024/03/04 17:50:09 minetest Exp $
+-- $Id: init.lua,v 1.8 2024/03/05 02:55:10 minetest Exp $
 --
 -- mod too add more fachwerk nodes via the API
 --
@@ -10,7 +10,7 @@
 -- Licence: AGPL
 --
 
-local vc_version = "$Id: init.lua,v 1.5 2024/03/04 17:50:09 minetest Exp $"
+local vc_version = "$Id: init.lua,v 1.8 2024/03/05 02:55:10 minetest Exp $"
 
 local vc_v = vc_version:split(" ",false,5,false)
 
@@ -35,6 +35,11 @@ end
 
 
 local additions = {
+
+   -- default
+   -- "default:",
+
+   -- bakedclay
    "bakedclay:white",
    "bakedclay:grey",
    "bakedclay:black",
@@ -50,6 +55,39 @@ local additions = {
    "bakedclay:pink", 
    "bakedclay:dark_grey",
    "bakedclay:dark_green",
+
+   --  minetest_errata
+   "minetest_errata:flint_block",
+   "minetest_errata:mossystone",
+   "default:mossycobble",
+   "minetest_errata:mossy_stone_tile",
+   "minetest_errata:desert_sandstone_cobble",
+   "minetest_errata:sandstone_cobble",
+   "minetest_errata:silver_sandstone_cobble",
+
+   -- moreblocks
+   "moreblocks:circle_stone_bricks",
+   "moreblocks:grey_bricks",
+   "moreblocks:iron_stone",
+   "moreblocks:coal_stone",
+   "moreblocks:all_faces_acacia_tree",
+   "moreblocks:all_faces_aspen_tree",
+   "moreblocks:all_faces_jungle_tree",
+   "moreblocks:all_faces_pine_tree",
+   "moreblocks:all_faces_tree",
+
+   -- cottages
+   "cottages:roof_vertical_brown",
+   "cottages:roof_vertical_shingle",
+   "cottages:loam",
+
+   -- swamp
+   "swamp:mud_block",
+   "swamp:mud_brick",
+
+
+   --   "",
+
 }
 
 
